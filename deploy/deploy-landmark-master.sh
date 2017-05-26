81788da706fe99875c55b868a539c0b5e4024678
andiwinata <andi.winata@hotmail.com>
2017-05-26 15:41:57 +1000

    Adding more echo

--- deploy/deploy-landmark-master.sh
+++ deploy/deploy-landmark-master.sh
@@ -5,48 +5,48 @@ set -e
 ## PUSH TO XDB-LANDMARK-DIST
 
 ## set up environment ##
-echo -e "Setting environment"
+echo -e "\nSetting environment\n"
 git --version
 git config --global user.email "landmark@fairfaxmedia.com.au"
 git config --global user.name "FFX UI Engineering Bot"
 git config --global push.default simple
 
 ## clone dependent source repos
-echo -e "Clone dependent source repos"
+echo -e "\nClone dependent source repos\n"
 cd ~/src/bitbucket.org/fairfax/xdb-landmark
 git clone git@bitbucket.org:fairfax/xdb-landmark-dist.git dist
 
 ## npm install
-echo -e "NPM install"
+echo -e "\nNPM install\n"
 rm package.json || export NO_FILE=1
 ln -s build/package.json package.json
 npm install
 
 ## get commit ##
-echo -e "Get commit"
+echo -e "\nGet commit\n"
 export LANDMARK_COMMIT=`git show --format="%h" HEAD | head -n1 | awk '{ print $1 }'`
 echo $LANDMARK_COMMIT
 export LANDMARK_COMMIT_MESSAGE=`git show --format="%B" HEAD | head -n1 | awk '{ print }'`
 echo $LANDMARK_COMMIT_MESSAGE
 
 ## clean dist subdirectories ##
-echo -e "Clean dist subdirectories"
+echo -e "\nClean dist subdirectories\n"
 cd dist/
 ls -1 -d */ | xargs rm -r -f
 
 ## package landmark ##
-echo -e "Package landmark"
+echo -e "\nPackage landmark\n"
 cd ../build/
 ln -s ../node_modules node_modules
 npm run grunt package
 
 ## get current version ##
-echo -e "Get LMK Version"
+echo -e "\nGet LMK Version\n"
 export LMK_VERSION=`git describe --abbrev=0`
 echo $LMK_VERSION
 
 ## commit and push changes to dist repo ##
-echo -e "commit and push changes to dist repo"
+echo -e "\ncommit and push changes to dist repo\n"
 cd ../dist/
 git status
 git add --all
@@ -56,32 +56,32 @@ export DIST_HAS_UPDATE=1
 
 ## if commit success DIST_HAS_UPDATE stays as 1 otherwise set DIST_HAS_UPDATE to 0
 git commit -m "Update from Landmark $LANDMARK_COMMIT" -m "$LANDMARK_COMMIT_MESSAGE" || DIST_HAS_UPDATE=0
-echo -e "Dist has update: $DIST_HAS_UPDATE"
+echo -e "\nDist has update: $DIST_HAS_UPDATE\n"
 
 ## If updates exist, push change ...
 if [[ "$DIST_HAS_UPDATE" = "1" ]]; then git push; fi
 
 ## Set remote branch
 export REMOTE_BRANCH="origin/$CI_BRANCH"
-echo -e "Remote branch: $REMOTE_BRANCH"
+echo -e "\nRemote branch: $REMOTE_BRANCH\n"
 
 if [[ "$DIST_HAS_UPDATE" = "1" ]]; then
     ## Checkout master on xdb-landmark
-    echo -e "Checkout master on xdb-landmark"
+    echo -e "\nCheckout master on xdb-landmark\n"
     cd ..
     git fetch origin
     git branch -a
     git reset --hard $REMOTE_BRANCH
 
     ## then bump version on xdb-landmark-dist
-    echo -e "bump version on xdb-landmark-dist"
+    echo -e "\nbump version on xdb-landmark-dist\n"
     cd dist/
     npm i
     grunt bump
     export NEW_VERSION=`git describe --abbrev=0`
 
     ## then tag version on xdb-landmark
-    echo -e "New version: $NEW_VERSION"
+    echo -e "\nTagging new version: $NEW_VERSION\n"
     cd ../
     git tag $NEW_VERSION
     git push origin $NEW_VERSION
@@ -91,13 +91,15 @@ fi
 
 ## if dist is updated ("$DIST_HAS_UPDATE" = "1") then do all the commands to bump up the ucms css repo
 if [[ "$DIST_HAS_UPDATE" = "1" ]]; then
+    echo -e "\nBUMPING UP UCMS CSS VERSION\n"
+
     ## cleaning local ucms-css repo
-    echo -e "cleaning local ucms-css repo"
+    echo -e "\ncleaning local ucms-css repo\n"
     rm -rf ~/src/bitbucket.org/fairfax/ucms-css
     mkdir -p ~/src/bitbucket.org/fairfax/ucms-css
 
     ## cloning ucms-css repo
-    echo -e "cloning ucms-css repo"
+    echo -e "\ncloning ucms-css repo\n"
     cd ~/src/bitbucket.org/fairfax/ucms-css
     git clone git@bitbucket.org:fairfax/ucms-css.git .
 
@@ -114,7 +116,7 @@ if [[ "$DIST_HAS_UPDATE" = "1" ]]; then
     for sitename in "${arr[@]}"
     do
         ## go to site folder
-        echo -e "go to site $sitename"
+        echo -e "\ngo to site $sitename\n"
         cd "./$sitename"
 
         ## replacing any tag in build gradle with the newest tag
@@ -131,7 +133,7 @@ if [[ "$DIST_HAS_UPDATE" = "1" ]]; then
     done  
 
     ## commit the changes
-    echo -e "commit the changes"
+    echo -e "\ncommit the changes to ucms css\n"
     git status
     git add --all
     git commit -m "Bump version from Landmark $LANDMARK_COMMIT" -m "$LANDMARK_COMMIT_MESSAGE"
